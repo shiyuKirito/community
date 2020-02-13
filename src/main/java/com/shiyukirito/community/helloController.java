40eeb0233c7fa9e9e5906c83408ea8f8858518c4
shiyukirito <994312551@qq.com>
2020-02-13 19:40:50 +0800

    add index

--- src/main/java/com/shiyukirito/community/helloController.java
+++ src/main/java/com/shiyukirito/community/helloController.java
@@ -1,16 +1,17 @@
 package com.shiyukirito.community;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class helloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name,Model model){
-        model.addAttribute("name",name);
-        return "hello";
+//    @GetMapping("/")
+//    public String hello(@RequestParam(name="name") String name,Model model){
+//        model.addAttribute("name",name);
+//        return "index";
+//    }
+    @GetMapping("/")
+    public  String index(){
+        return "index";
     }
-
 }
